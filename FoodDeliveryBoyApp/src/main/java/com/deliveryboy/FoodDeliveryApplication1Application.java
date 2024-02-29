863a57d6a61df8649cb1605c1f88ea01146cc0ee
subhajit51193 <nnorth87@gmail.com>
2024-02-29 21:55:30 +0530

    deliveryBoy service created

---
+++ FoodDeliveryBoyApp/src/main/java/com/deliveryboy/FoodDeliveryApplication1Application.java
@@ -0,0 +1,13 @@
+package com.deliveryboy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryApplication1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodDeliveryApplication1Application.class, args);
+	}
+
+}
